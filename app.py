e40007eb12022cb14b122048fce50eb5f63ce8d4
Darlene Wu <darlenewu1310@gmail.com>
2026-08-24 02:10:34 -0400

    could welcome people + add them to the pikmins user group

---
+++ app.py
@@ -0,0 +1,86 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+app = App(token=os.getenv("SLACK_BOT_TOKEN"))
+
+CHANNEL_ID = "C0819J5AFPS"
+USERGROUP_ID ="S08AXRM7BGE"
+
+def add_to_usergroup(client, user_id):
+    current = client.usergroups_users_list(usergroup=USERGROUP_ID)
+    if not current["ok"]:
+        print(f"[usergroups_users_list failed] {current}")
+        return
+    
+    member_ids = current["users"]
+
+    if user_id in member_ids:
+        print(f"{user_id} is already in the usergroup, skipping")
+        return
+
+    member_ids.append(user_id)
+    result = client.usergroups_users_update(usergroup=USERGROUP_ID, users=member_ids)
+
+    if not result["ok"]:
+        print(f"[usergroup_users_list failed] {result}")
+    else:
+        print(f"added {user_id} to usergroup {USERGROUP_ID}")
+
+@app.event("member_joined_channel")
+def welcome(event, client):
+    channel = event["channel"]
+
+    if channel != CHANNEL_ID:
+        return
+
+    user_id = event["user"]
+    add_to_usergroup(client, user_id)
+    text = f"wahhhh! welcome <@{user_id}> :D"
+
+    client.chat_postMessage(
+        channel=channel,
+        text=text,
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": text,
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "clicky",
+                            "emoji": True
+                        },
+                        "action_id": "send_emojis"
+                    }
+                ]
+            }
+        ]
+    )
+
+@app.action("send_emojis")
+def send_emojis(ack, body, client):
+    ack()
+
+    channel = body["channel"]["id"]
+    ts = body["message"]["ts"]
+
+    client.chat_postMessage(
+        channel=channel,
+        thread_ts=ts,
+        text=":red-pikmin:"
+    )
+
+if __name__ == "__main__":
+    print("bot is running!")
+    SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN")).start()
